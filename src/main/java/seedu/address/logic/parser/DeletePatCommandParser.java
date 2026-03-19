a5a8575c29ebe17269fa6134fdb7504950994e61
TanShiiJiee <e1356537@u.nus.edu>
2026-03-19 15:31:56 +0800

    Add test cases for delpat

--- src/main/java/seedu/address/logic/parser/DeletePatCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePatCommandParser.java
@@ -9,7 +9,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new DeletePatCommand object
  */
-public class DeletePatCommandParser {
+public class DeletePatCommandParser implements Parser<DeletePatCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeletePatCommand
      * and returns a DeletePatCommand object for execution.
